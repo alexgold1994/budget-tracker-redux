ef06824f0dc744bc787f32f1997732ea4532bcbc
Alex <alexgold1994@gmail.com>
2021-04-25 16:27:23 +0300

    add select with filter by date and amount

--- src/components/expenseFilterInput.js
+++ src/components/expenseFilterInput.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ExpenseListItem from './expenseListItem';
 import {connect} from 'react-redux';
-import {setKeywordFilter} from '../actions/filterActions';
+import {setKeywordFilter, sortByAmount, sortByDate} from '../actions/filterActions';
 
 const ExpenseFilterInput = (props) => (
     <div>
-        <input type="text" 
-        value={props.filters.keyword} 
-        onChange={(e) => {
-            props.dispatch(setKeywordFilter(e.target.value))
-        }}
+        <input
+            type="text" 
+            value={props.filters.keyword} 
+            onChange={(e) => {
+                props.dispatch(setKeywordFilter(e.target.value))
+            }}
         />
+        <select
+            value={props.filters.sortBy}
+            onChange={(e) => {
+                if (e.target.value === 'date') {
+                    props.dispatch(sortByDate());
+                }else if (e.target.value === 'amount'){
+                    props.dispatch(sortByAmount());
+                }
+            }}
+        >
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+        </select>
+
     </div>
 )
 
